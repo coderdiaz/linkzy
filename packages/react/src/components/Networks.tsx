18ff7f5501a64d603a677c69a0404cac229fde7f
Javier Diaz <coderdiaz@gmail.com>
2023-11-11 13:47:59 -0600

    fix: removed non required component

--- packages/react/src/components/Networks.tsx
+++ packages/react/src/components/Networks.tsx
@@ -1,14 +1,6 @@
 import type { CommonProps } from '../helpers/props/common.prop';
 
 /**
- * Networks
- */
-interface NetworksProps extends CommonProps {}
-export const Networks = ({ children, className }: NetworksProps) => {
-  return <div className={className}>{children}</div>;
-};
-
-/**
  * Network
  */
 interface NetworkProps
